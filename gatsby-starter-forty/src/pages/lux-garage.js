a44b86f9e780ca4431bf0ea10dcb70807b2a85c3
danielmonk <danielmonk@msn.com>
2020-07-15 18:40:25 +0100

    fixing case on images

--- gatsby-starter-forty/src/pages/lux-garage.js
+++ gatsby-starter-forty/src/pages/lux-garage.js
@@ -7,10 +7,10 @@ import BannerGarage from '../components/BannerGarage'
 import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
 import pic10 from '../assets/images/pic10.jpg'
-import gallerypic1 from '../assets/images/IMG_1644.jpg'
-import gallerypic2 from '../assets/images/IMG_2125.jpg'
-import gallerypic3 from '../assets/images/IMG_2132.jpg'
-import gallerypic4 from '../assets/images/IMG_2133.jpg'
+import gallerypic1 from '../assets/images/img_1644.jpg'
+import gallerypic2 from '../assets/images/img_2125.jpg'
+import gallerypic3 from '../assets/images/img_2132.jpg'
+import gallerypic4 from '../assets/images/img_2133.jpg'
 
 const Landing = (props) => (
     <Layout>
