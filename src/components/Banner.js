74de9103e93077d7e023af1cd290cd1b39b7d7cc
danielmonk <danielmonk@msn.com>
2020-07-16 17:10:20 +0100

    =removing unneeded directory

---
+++ src/components/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h2 className="boxed">The home of <strong>Luxury</strong> <span>domestic</span> and <span>commercial</span> floors</h2>
+            </header>
+            <div className="content">
+            <p>We specialise in supplying and fitting professional wood & other easy-to-fit floor coverings.</p>
+                <ul className="actions">
+                    <li><a href="#contact" className="button next scrolly">Enquire now</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
