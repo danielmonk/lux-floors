24a92b779ce6bc37cde5ecf7e7062dea57ed5fb9
danielmonk <danielmonk@msn.com>
2020-07-20 18:11:46 +0100

    fixed untracked files

---
+++ .cache/async-requires.js
@@ -0,0 +1,17 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-contact-js": () => import("./../src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-elements-js": () => import("./../src/pages/elements.js" /* webpackChunkName: "component---src-pages-elements-js" */),
+  "component---src-pages-gallery-js": () => import("./../src/pages/gallery.js" /* webpackChunkName: "component---src-pages-gallery-js" */),
+  "component---src-pages-generic-js": () => import("./../src/pages/generic.js" /* webpackChunkName: "component---src-pages-generic-js" */),
+  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-landing-js": () => import("./../src/pages/landing.js" /* webpackChunkName: "component---src-pages-landing-js" */),
+  "component---src-pages-lux-floors-js": () => import("./../src/pages/lux-floors.js" /* webpackChunkName: "component---src-pages-lux-floors-js" */),
+  "component---src-pages-lux-garage-js": () => import("./../src/pages/lux-garage.js" /* webpackChunkName: "component---src-pages-lux-garage-js" */),
+  "component---src-pages-success-js": () => import("./../src/pages/success.js" /* webpackChunkName: "component---src-pages-success-js" */)
+}
+
